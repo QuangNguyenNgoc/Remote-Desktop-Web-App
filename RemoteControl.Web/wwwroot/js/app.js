8c53d18fae630e8d1b696ba1ee42bb6897a8d344
QuangNguyenNgoc <93335956+QuangNguyenNgoc@users.noreply.github.com>
2025-12-17 15:58:20 +0700

    utils: save file as type function

---
+++ RemoteControl.Web/wwwroot/js/app.js
@@ -0,0 +1,14 @@
+// Utility functions for Blazor JS interop
+
+// Download file to user's device
+window.downloadFile = function (fileName, content) {
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
